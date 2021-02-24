635d0f677b7052c5353c746191d1331ea7c4a45e
sidd sirius <sidd.benoit@gmail.com>
2021-02-24 12:24:51 -0500

    material-ui install

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -1,14 +1,14 @@
 import './BtnAjoutRessource.scss';
 import Fab from '@material-ui/core/Fab';
-// import AddIcon from '@material-ui/icons/Add';
+import AddIcon from '@material-ui/icons/Add';
 
 export default function BtnAjoutRessource() {
   return (
     // <button className="BtnAjoutRessource">
     //   Ajouter
     // </button>
-    <button className="BtnAjoutRessource">
-      +
-    </button>
+    <Fab className="BtnAjoutRessource">
+      <AddIcon />
+    </Fab>
   );
 }
