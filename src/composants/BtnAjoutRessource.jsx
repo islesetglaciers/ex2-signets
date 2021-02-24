dfea04c385227c1f20ac9f44317e54aad24f6ab1
sidd sirius <sidd.benoit@gmail.com>
2021-02-24 12:19:47 -0500

    début scss

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -1,7 +1,14 @@
+import './BtnAjoutRessource.scss';
+import Fab from '@material-ui/core/Fab';
+// import AddIcon from '@material-ui/icons/Add';
+
 export default function BtnAjoutRessource() {
   return (
+    // <button className="BtnAjoutRessource">
+    //   Ajouter
+    // </button>
     <button className="BtnAjoutRessource">
-      Ajouter
+      +
     </button>
   );
 }
