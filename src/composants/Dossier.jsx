27a5c3892ce186a9b43afa4d020c3ca596cd1f36
sidd sirius <sidd.benoit@gmail.com>
2021-02-25 14:06:35 -0500

    fix bg couleur

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -4,7 +4,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 export default function Dossier({id, titre, couleur, dateModif}) {
   return (
-    <article className="Dossier" style={{ backgroundColor: {couleur} }}>
+    <article className="Dossier" style={{ backgroundColor: couleur }}>
       <div className="couverture">
         <span className="deplacer">
           <SortIcon size="large"/>
